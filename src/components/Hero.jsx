4e2d4c3e82eeac71603956ec131b8d16cf4726c3
Parth Singh <parthsingh503@gmail.com>
2025-03-05 00:04:16 +0530

    Update Hero component to use new Parth image and adjust image styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { LuImport } from "react-icons/lu"
-import benson from "../assets/benson.webp"
+//import benson from "../assets/benson.webp"
 import parth from "../assets/Parthpic8.jpg"
 import { AnimatePresence,motion } from "motion/react";
 import FloatingIcons from "./FloatingIcons";
@@ -58,7 +58,7 @@ function Hero() {
                     animate={{ opacity: 1, y:0}}
                     transition={{duration:0.5, delay:2}}
                     className="w-full">
-                        <img src={parth} alt="Parth Singh" className="mt-8 h-96 w-full object-cover"/>
+                        <img src={parth} alt="Parth Singh" className="mt-8 h-96 w-full object-cover lg:object-[0px_-8em]"/>
                     </motion.div>
                 </div>
             </section>
